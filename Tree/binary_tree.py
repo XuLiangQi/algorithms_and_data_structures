a80d479eaa6332188d9280160cfa4b7a022faccd
XuLiang Qi <76532517+XuLiangQi@users.noreply.github.com>
2023-10-09 14:13:01 -0400

    adding find method

--- Tree/binary_tree.py
+++ Tree/binary_tree.py
@@ -14,12 +14,15 @@ class BinaryTree:
                 root_node.right_node = new_node
             else:
                 self.assign_node(root_node.right_node, new_node)
-        else:
+        elif new_node.value < root_node.value:
             if root_node.left_node is None:
                 root_node.left_node = new_node
             else:
                 self.assign_node(root_node.left_node, new_node)
-
+        else:
+            print("BinaryTree::assign_node : Duplicate found, cannot assign duplicate node.")
+            return
+        
 
     def add(self, value):
         this_node = Node(value)
@@ -28,7 +31,21 @@ class BinaryTree:
         else:
             self.assign_node(self.root, this_node)
 
+    def find(self, current_node, value):
+        if value > current_node.value:
+            current_node = self.find(current_node.right_node, value)
+        elif value < current_node.value:
+            current_node = self.find(current_node.left_node, value)
+        return current_node
 
+    def delete(self, value):
+        # this_node = self.find(self.root, value)
+        # if this_node.left_node is None and this_node.right_node is None:
+        pass
+
+
+    def show(self):
+        pass
 
 b = BinaryTree()
 b.add(5)
